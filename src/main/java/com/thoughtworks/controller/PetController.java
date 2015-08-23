9a5c908ea454e9ea28ac8b670ce3c48a7df74ae9
kolyjjj <me.koly.li@gmail.com>
2015-08-24 07:59:55 +0800

    add spring jpa and h2 database

--- src/main/java/com/thoughtworks/controller/PetController.java
+++ src/main/java/com/thoughtworks/controller/PetController.java
@@ -3,6 +3,8 @@ package com.thoughtworks.controller;
 import com.google.common.collect.Lists;
 import com.thoughtworks.model.Gender;
 import com.thoughtworks.model.Pet;
+import com.thoughtworks.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,16 @@ import java.util.List;
 @RequestMapping("/api/pets")
 public class PetController {
 
+    private PetRepository petRepository;
+
+    @Autowired
+    public PetController(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Pet> list() {
+        petRepository.findAll();
         return Lists.newArrayList(new Pet("Puppy", Gender.MALE, "dog"), new Pet("Brain", Gender.FEMALE, "cat"));
     }
 }
--- src/main/java/com/thoughtworks/model/Pet.java
+++ src/main/java/com/thoughtworks/model/Pet.java
@@ -1,6 +1,16 @@
 package com.thoughtworks.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
     public String name;
     public Gender gender;
     public String type;
