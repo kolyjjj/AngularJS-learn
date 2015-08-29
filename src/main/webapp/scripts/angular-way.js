fbf153bb0f454c5f0547aa4e9594d6101c1eca6e
kolyjjj <me.koly.li@gmail.com>
2015-08-29 18:41:31 +0800

    refactor list pets to a service

--- src/main/webapp/scripts/angular-way.js
+++ src/main/webapp/scripts/angular-way.js
@@ -1,9 +1,18 @@
 angular.module('app', []);
 
 angular.module('app')
-        .controller('oneController', ['$scope', '$http', function($scope, $http){
+        .controller('oneController', ['$scope', 'petService', function($scope, petService){
             $scope.input = '';
-            $http.get('/api/pets').then(function(response){
-                console.log('pets', response.data);
-            });
+            petService.pets();
+        }])
+        .factory('petService', ['$http', function($http){
+            return {
+                pets: pets
+            };
+
+            function pets(){
+                $http.get('/api/pets').then(function(response){
+                    console.log('pets', response.data);
+                });
+            }
         }]);
