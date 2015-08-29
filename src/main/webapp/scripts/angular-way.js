ff6862ae04b4808ca8bc6a32dfe365af8c027b60
kolyjjj <me.koly.li@gmail.com>
2015-08-29 18:17:16 +0800

    get pets using $http in controller

--- src/main/webapp/scripts/angular-way.js
+++ src/main/webapp/scripts/angular-way.js
@@ -1,6 +1,9 @@
 angular.module('app', []);
 
 angular.module('app')
-        .controller('oneController', ['$scope', function($scope){
+        .controller('oneController', ['$scope', '$http', function($scope, $http){
             $scope.input = '';
+            $http.get('/api/pets').then(function(response){
+                console.log('pets', response.data);
+            });
         }]);
